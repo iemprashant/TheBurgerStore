1ed684acdd604e254de13491791662cd58e2c18f
iemprashant <prashantgupta9876@gmail.com>
2020-05-21 00:37:09 +0530

    added order now button

--- src/Containers/BurgerBuilder/BurgerBuilder.js
+++ src/Containers/BurgerBuilder/BurgerBuilder.js
@@ -17,15 +17,27 @@ class BurgerBuilder extends Component {
             cheese:0,
             meat:0
         },
-        totalPrice:4
+        totalPrice:4,
+        purchasable : false
     }
     
+    updatePurchaseState (ingredients) {
+        const sum = Object.keys( ingredients )
+            .map( igKey => {
+                return ingredients[igKey];
+            } )
+            .reduce( ( sum, el ) => {
+                return sum + el;
+            }, 0 );
+        this.setState( { purchasable: sum > 0 } );
+    }
 
     addingredienthandler=(type)=>{
         const updatedIngredients = {...this.state.ingredients};
         updatedIngredients[type]=this.state.ingredients[type]+1;
         const newPrice = this.state.totalPrice + INGREDIENTS_PRICES[type];
         this.setState({totalPrice:newPrice , ingredients: updatedIngredients})
+        this.updatePurchaseState(updatedIngredients);
     };
     removeingredienthandler=(type)=>{
         const updatedIngredients = {...this.state.ingredients};
@@ -35,6 +47,7 @@ class BurgerBuilder extends Component {
         updatedIngredients[type]=this.state.ingredients[type]-1;
         const newPrice = this.state.totalPrice - INGREDIENTS_PRICES[type];
         this.setState({totalPrice:newPrice , ingredients: updatedIngredients})
+        this.updatePurchaseState(updatedIngredients);
     }; 
 
     render() {
@@ -52,7 +65,8 @@ class BurgerBuilder extends Component {
                 ingredientAdded={this.addingredienthandler}
                 ingredientRemoved={this.removeingredienthandler}
                 disabled={disabledInfo}
-                price={this.state.totalPrice}/>
+                price={this.state.totalPrice}
+                purchasable={this.state.purchasable}/>
             </Fragment>
         );
     }
