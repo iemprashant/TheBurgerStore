cdd57f46cb6e498d14744294b5b925b08a71304e
iemprashant <prashantgupta9876@gmail.com>
2020-07-28 22:14:25 +0530

    fix logout issue

--- src/App.js
+++ src/App.js
@@ -1,62 +1,60 @@
-import React, { useEffect,Suspense } from 'react';
-import './App.css';
-import BurgerBuilder from './Containers/BurgerBuilder/BurgerBuilder';
-import Layout from './Containers/Layout/Layout';
-import { Route, Switch, withRouter, Redirect } from 'react-router';
-import logout from './Containers/Auth/Auth/Logout';
-import * as actions from './store/actions/index'
-import { connect } from 'react-redux';
-const Checkout =React.lazy(()=>{
-  return import ('./Containers/Checkout/Checkout')
-})
-const Orders = React.lazy(()=>{
-  return import ('./Containers/Orders/Orders')
-})
-const Auth = React.lazy(()=>{
-  return import ('./Containers/Auth/auth')
-})
-const App= (props)=>{
-  const {onTryAutoSignup}=props;
+import React, { useEffect, Suspense } from "react";
+import BurgerBuilder from "./Containers/BurgerBuilder/BurgerBuilder";
+import Layout from "./Containers/Layout/Layout";
+import { Route, Switch, withRouter, Redirect } from "react-router";
+import logout from "./Containers/Auth/Auth/Logout";
+import * as actions from "./store/actions/index";
+import { connect } from "react-redux";
+const Checkout = React.lazy(() => {
+  return import("./Containers/Checkout/Checkout");
+});
+const Orders = React.lazy(() => {
+  return import("./Containers/Orders/Orders");
+});
+const Auth = React.lazy(() => {
+  return import("./Containers/Auth/auth");
+});
+const App = (props) => {
+  const { onTryAutoSignup } = props;
   useEffect(() => {
     onTryAutoSignup();
   }, [onTryAutoSignup]);
-    let routes = (
+  let routes = (
+    <Switch>
+      <Route path="/auth" render={(props) => <Auth {...props} />} />
+      <Route path="/" exact component={BurgerBuilder} />
+      <Redirect to="/" />
+    </Switch>
+  );
+  if (props.isAuthenticated) {
+    routes = (
       <Switch>
-        <Route path="/auth" render={(props)=><Auth {...props}/>}/>
-        <Route path="/" exact component={BurgerBuilder}/>
-        <Redirect to ='/'/>
+        <Route path="/Checkout" render={(props) => <Checkout {...props} />} />
+        <Route path="/orders" render={(props) => <Orders {...props} />} />
+        <Route path="/logout" component={logout} />
+        <Route path="/auth" render={(props) => <Auth {...props} />} />
+        <Route path="/" exact component={BurgerBuilder} />
+        <Redirect to="/" />
       </Switch>
     );
-    if (props.isAuthenticated){
-      routes=(
-        <Switch>
-        <Route path="/Checkout" render={(props)=><Checkout {...props}/>}/>
-        <Route path="/orders" render={(props)=><Orders{...props}/>}/>
-        <Route path="/logout" component={logout}/>
-        <Route path="/auth" render={(props)=><Auth{...props}/>}/>
-        <Route path="/" exact component={BurgerBuilder}/>
-        <Redirect to ='/'/>
-      </Switch>  
-      );
-    }
-    return (
-      <div>
-        <Layout>
-          <Suspense fallback={<p>Loading...</p>}>
-          {routes}
-          </Suspense>
-        </Layout>
-      </div>
-    )
-}
-const mapStateToProps = state =>{
+  }
+  return (
+    <div>
+      <Layout>
+        <Suspense fallback={<p>Loading...</p>}>{routes}</Suspense>
+      </Layout>
+    </div>
+  );
+};
+const mapStateToProps = (state) => {
   return {
-      isAuthenticated :state.auth.token!==null
-}};
-const mapDispatchToprops=dispatch =>{
-  return {
-    onTryAutoSignup:()=>dispatch(actions.authCheckState())
+    isAuthenticated: state.auth.token !== null,
   };
-}
+};
+const mapDispatchToprops = (dispatch) => {
+  return {
+    onTryAutoSignup: () => dispatch(actions.authCheckState()),
+  };
+};
 
-export default withRouter(connect(mapStateToProps,mapDispatchToprops)(App));
+export default withRouter(connect(mapStateToProps, mapDispatchToprops)(App));
--- src/Components/Burger/Burger.js
+++ src/Components/Burger/Burger.js
@@ -1,28 +1,29 @@
-import React from 'react'
-import  classes from './Burger.module.css'
-import BurgerIngredient from './BurgerIngredient/Burgeringredient'
+import React from "react";
+import classes from "./Burger.module.css";
+import BurgerIngredient from "./BurgerIngredient/Burgeringredient";
 
+const Burger = (props) => {
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map((igkey) => {
+      return [...Array(props.ingredients[igkey])].map((_, i) => {
+        return <BurgerIngredient key={igkey + i} type={igkey} />;
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
 
-const Burger= (props) => {
-    let transformedIngredients=Object.keys(props.ingredients)
-        .map(igkey=>{
-            return[...Array(props.ingredients[igkey])].map((_,i)=>{
-                return <BurgerIngredient key= {igkey+i} type={igkey}/>;
-            });
-        })
-        .reduce((arr,el)=>{ return arr.concat(el) },[]);
+  if (transformedIngredients == 0) {
+    transformedIngredients = <p>Please start adding ingredients!</p>;
+  }
 
-    if(transformedIngredients == 0){
-        transformedIngredients = <p>Please start adding ingredients!</p>
-    }
+  return (
+    <div className={classes.Burger}>
+      <BurgerIngredient type="bread-top" />
+      {transformedIngredients}
+      <BurgerIngredient type="bread-bottom" />
+    </div>
+  );
+};
 
-    return (
-        <div className={classes.Burger}>
-            <BurgerIngredient type="bread-top"/>
-            {transformedIngredients}
-            <BurgerIngredient type="bread-bottom"/>
-        </div>
-    );
-}
-
-export default Burger
+export default Burger;
